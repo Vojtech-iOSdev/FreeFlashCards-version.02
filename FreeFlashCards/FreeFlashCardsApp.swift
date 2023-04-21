67c7de96d99c4171df5fd4557bed1f63a2226620
Vojtěch Kalivoda <kalivoda167@gmail.com>
2023-04-21 13:27:32 +0200

    Initial Commit

---
+++ FreeFlashCards/FreeFlashCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FreeFlashCardsApp.swift
+//  FreeFlashCards
+//
+//  Created by Vojtěch Kalivoda on 21.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct FreeFlashCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
